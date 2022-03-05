2e6271dbbab76e3868a6cee1c9a800d87ccd30a1
osy <osy@turing.llc>
2022-03-04 19:17:58 -0800

    red-stream: disable socket_set_cork() on Darwin
    TCP_NOPUSH is broken and cannot be used.

--- server/red-stream.c
+++ server/red-stream.c
@@ -42,7 +42,7 @@
 #include "websocket.h"
 
 // compatibility for *BSD systems
-#if !defined(TCP_CORK) && !defined(_WIN32)
+#if !defined(TCP_CORK) && !defined(_WIN32) && !defined(__APPLE__)
 #define TCP_CORK TCP_NOPUSH
 #endif
 
@@ -105,7 +105,8 @@ struct RedStreamPrivate {
     SpiceCoreInterfaceInternal *core;
 };
 
-#ifndef _WIN32
+// TCP_NOPUSH is broken on Darwin
+#if !defined(_WIN32) && !defined(__APPLE__)
 /**
  * Set TCP_CORK on socket
  */
